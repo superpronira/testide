3ffc469b88a1479a882978c5061d716a38305bca
amduser <superpronira@yandex.ru>
2025-12-03 22:40:44 +0300

    модифицировал класс SimpleUtil

--- src/main/java/ru/dimasoft/testide/SimpleUtil.java
+++ src/main/java/ru/dimasoft/testide/SimpleUtil.java
@@ -2,6 +2,9 @@ package ru.dimasoft.testide;
 
 public class SimpleUtil {
     private String str;
+    private int version;
+    private String name;
+
 
     public String getStr() {
         return str;
@@ -9,5 +12,21 @@ public class SimpleUtil {
 
     public void setStr(String str) {
         this.str = str;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
