c93920830b8f99f0852e72787c06081e39da67d5
amduser <superpronira@yandex.ru>
2025-12-03 20:00:58 +0300

    initial commit

---
+++ src/main/java/ru/dimasoft/testide/TestideApplication.java
@@ -0,0 +1,14 @@
+package ru.dimasoft.testide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestideApplication.class, args);
+        System.out.println("первое приложение в idea 25");
+    }
+
+}
---
+++ src/test/java/ru/dimasoft/testide/TestideApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dimasoft.testide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
